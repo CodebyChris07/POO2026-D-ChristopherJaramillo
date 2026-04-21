b0f345623412515d2197019acc10614a294e131f
CodebyChris07 <chrisjalu07@gmail.com>
2026-04-21 16:53:12 -0500

    ejercicios con retornos

---
+++ ejemplosRecursividad/src/ejemplosrecursividad/EjemplosRecursividad.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplosrecursividad;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class EjemplosRecursividad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese un numero para factorial");
+        int num = entrada.nextInt();
+        int m = factorial(num);
+        System.out.println("Ingrese un numero para suma");
+        int suma = entrada.nextInt();
+        int s = suma(suma);
+        System.out.println("Ingrese primer numero para suma");
+        int s1 = entrada.nextInt();
+        System.out.println("Ingrese segundo numero para suma");
+        int s2 = entrada.nextInt();
+        int suma23 = suma2(s1, s2);
+        System.out.println(m + "\n" + s + "\n" + suma23);
+
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+
+            return 1;
+
+        } else {
+
+            return (n * factorial(n - 1));
+        }
+
+    }
+
+    public static int suma(int n) {
+        if (n == 1) {
+
+            return 1;
+
+        } else {
+
+            return (n + suma(n - 1));
+        }
+
+    }
+
+    public static int suma2(int start, int end) {
+
+        if (start < end) {
+
+            return start;
+
+        } else {
+
+            return (start + suma2(start + 1, end));
+
+        }
+
+    }
+
+}
